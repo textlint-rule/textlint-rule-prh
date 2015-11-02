9a18cc64b65200bcefd0bba073010ce100036374
azu <azuciao@gmail.com>
2015-11-02 11:37:57 +0900

    test(prh): add import test

--- test/textlintrc-test.js
+++ test/textlintrc-test.js
@@ -1,42 +1,60 @@
 // LICENSE : MIT
 "use strict";
 import assert from "power-assert";
-import {textlint} from "textlint";
+import {TextLintCore} from "textlint";
 import rule from "../src/prh-rule";
 import path from "path";
 describe(".textlinrc test", function () {
     context("when use .textlintrc", function () {
         it("should resolve path to rule.yaml", function () {
+            var textlint = new TextLintCore();
             textlint.setupRules({
                 "prh": rule
             }, {
                 "prh": {
                     "rulePaths": [path.join(__dirname, "fixtures", "rule.yaml")]
                 }
-            }, {
-                "configFile": path.join(__dirname, "fixtures", ".textlintrc")
             });
             var result = textlint.lintMarkdown("jquery");
             assert(result.messages.length === 1);
             assert(result.messages[0].line === 1);
             assert(result.messages[0].column === 1);
-            textlint.resetRules();
         });
         it("should resolve path to rule.yaml", function () {
+            var textlint = new TextLintCore();
             textlint.setupRules({
                 "prh": rule
             }, {
                 "prh": {
                     "rulePaths": [path.join(__dirname, "fixtures", "rule.yaml")]
                 }
-            }, {
-                "configFile": path.join(__dirname, "fixtures", ".textlintrc")
             });
             var result = textlint.lintMarkdown("jquery");
             assert(result.messages.length === 1);
             assert(result.messages[0].line === 1);
             assert(result.messages[0].column === 1);
-            textlint.resetRules();
+        });
+    });
+    context("prh features", function () {
+        describe("import", function () {
+            it("should work import directive", function () {
+                var textlint = new TextLintCore();
+                textlint.setupRules({
+                    "prh": rule
+                }, {
+                    "prh": {
+                        "rulePaths": [path.join(__dirname, "fixtures", "imports.yml")]
+                    }
+                });
+                var result = textlint.lintMarkdown("A");
+                assert(result.messages.length === 1);
+                var message = result.messages[0].message;
+                assert.equal(message, "A => a");
+                var resultB = textlint.lintMarkdown("B");
+                assert(resultB.messages.length === 1);
+                var messageB = resultB.messages[0].message;
+                assert.equal(messageB, "B => b");
+            });
         });
     });
 });
